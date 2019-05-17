309a21bd88b08326e0d811a451f73786d1bc3979
王松 <572764113@qq.com>
2019-05-17 17:46:18 +0800

    更改后台报错

--- back-end/app/service/user.js
+++ back-end/app/service/user.js
@@ -27,6 +27,7 @@
 const Service = require("egg").Service;
 class UserService extends Service {
   async login(reqParams) {
+    this.logger.debug("aaaa");
     const currentUserInfo = await this.app.mysql.get("mysql.user_test", {
       user_name: reqParams.userName
     });
